ca121d056d6efaa918d3fc158c242b85765cd73d
Clinton Dean <clinton@datameshgroup.com>
2023-05-18 15:41:12 +1000

    Bugfix for parsing TerminalEnvironment in FusionApp

--- DataMeshGroup.Fusion/Util/StringEnumConverterWithDefaultOrNull.cs
+++ DataMeshGroup.Fusion/Util/StringEnumConverterWithDefaultOrNull.cs
@@ -8,15 +8,20 @@ namespace DataMeshGroup.Fusion
     /// Specialized StringEnumConverter that returns the default enum value instead of throwing if the string
     /// cannot be matched to an enum value during deserialization.
     /// </summary>
-    public class StringEnumConverterWithDefault<TEnum> : StringEnumConverter where TEnum : struct
+    public class StringEnumConverterWithDefaultOrNull<TEnum> : StringEnumConverter where TEnum : struct
     {
-        public StringEnumConverterWithDefault()
+        public StringEnumConverterWithDefaultOrNull()
         {
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            string s = reader.Value?.ToString() ?? "Unknown";
+            if(reader?.Value == null)
+            {
+                return null;
+            }
+            
+            string s = reader.Value.ToString();
 
             // If value provided isn't available, try using "Unknown". If that doesn't work just use the default value
             if (!Enum.TryParse(s, ignoreCase: true, result: out TEnum t))
