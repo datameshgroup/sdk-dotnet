4fbcc1b62bbd29e748eb26636cca98f3a9321a9c
Grace <95324995+i-grace@users.noreply.github.com>
2024-01-18 11:22:27 +1100

    Updated versions to 1.7.0-rc2.  Added StoredValue references to Fusion.Model.  Added StoredValueResponse to RepeatedResponseMessageBody.

--- DataMeshGroup.Fusion/Model/RepeatedResponseMessageBody.cs
+++ DataMeshGroup.Fusion/Model/RepeatedResponseMessageBody.cs
@@ -3,5 +3,7 @@
     public class RepeatedResponseMessageBody
     {
         public PaymentResponse PaymentResponse { get; set; }
+
+        public StoredValueResponse StoredValueResponse { get; set; }
     }
 }
