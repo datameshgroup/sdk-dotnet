ab3d40dde36f853816591800c43a18968b063f3c
Grace <95324995+i-grace@users.noreply.github.com>
2024-01-30 11:33:27 +1100

    Updated versions to 1.8.0-rc6.  Added JsonIgnore tag to StoredValueDataItem.

--- DataMeshGroup.Fusion/Model/StoredValueRequest.cs
+++ DataMeshGroup.Fusion/Model/StoredValueRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -42,6 +43,7 @@ namespace DataMeshGroup.Fusion.Model
         /// <summary>
         /// Returns the first item in StoredValueData
         /// </summary>
+        [JsonIgnore]
         public StoredValueData StoredValueDataItem
         {
             get
