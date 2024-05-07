3591562b479dbc182a33eae9e907436ddf0c0adf
Grace <95324995+i-grace@users.noreply.github.com>
2024-05-07 10:57:09 +1000

    Release v1.8.5 - Updated POISoftware data type to List in POISystemData.

--- DataMeshGroup.Fusion/Model/POISystemData.cs
+++ DataMeshGroup.Fusion/Model/POISystemData.cs
@@ -1,9 +1,11 @@
-﻿namespace DataMeshGroup.Fusion.Model
+﻿using System.Collections.Generic;
+
+namespace DataMeshGroup.Fusion.Model
 {
     public class POISystemData
     {
         public string DateTime { get; set; }
-        public POISoftware POISoftware { get; set; }
+        public List<POISoftware> POISoftware { get; set; }
         public POITerminalData POITerminalData { get; set; }
         public POIStatus POIStatus { get; set; }
         public bool? TokenRequestStatus { get; set; }
